ffe90a7dc17c257eb87d7a5936868b96ead809a3
Augusto Alonso <augustoalonso98@gmail.comn>
2020-03-22 17:21:30 -0600

    l

--- resources/js/components/Navbar.js
+++ resources/js/components/Navbar.js
@@ -24,15 +24,15 @@ const Navbar = () => {
     const [showing, setShow] = useState(false)
     return (
         <nav className="navbar navbar-expand-lg navbar-light main-dark-bg p-3 ">
-            <a className="navbar-brand title-3 white" href="/">Streaming</a>
+            <a className="navbar-brand title-3 white" href="#">Streaming</a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" >
                 <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav mr-auto">
-                    <li className="nav-item active">
+                    {/* <li className="nav-item active">
                         <a className="nav-link subtitle-2 white" href="/">Home</a>
-                    </li>
+                    </li> */}
                     <li className="nav-item">
                         <a className="nav-link subtitle-2 white" href="/artists">Artistas</a>
                     </li>
